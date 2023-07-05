ccfb66fcfdf3f9df6dbb9938a9bcdf444d4bc42f
kinsomaz <Kinsomaz87@gmail.com>
2023-07-05 12:12:05 +0100

    update

---
+++ app/src/main/java/com/samad/recipebook/Unfollow_friend.kt
@@ -0,0 +1,11 @@
+package com.samad.recipebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Unfollow_friend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unfollow_friend)
+    }
+}
