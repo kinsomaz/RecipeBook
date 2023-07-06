4674d9ff8cd03817f33cb1e2542fe87085a2bf18
kinsomaz <Kinsomaz87@gmail.com>
2023-07-06 12:23:13 +0100

    update

---
+++ app/src/main/java/com/samad/recipebook/Networks.kt
@@ -0,0 +1,11 @@
+package com.samad.recipebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Networks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_networks)
+    }
+}
