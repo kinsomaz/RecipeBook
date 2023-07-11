10f7900bfd919d9614441065cad64ca128d40f1c
kinsomaz <Kinsomaz87@gmail.com>
2023-07-11 13:07:26 +0100

    update

--- app/src/main/java/com/samad/recipebook/SignUp.kt
+++ app/src/main/java/com/samad/recipebook/SignUp.kt
@@ -2,10 +2,45 @@ package com.samad.recipebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.samad.recipebook.databinding.ActivitySignUpBinding
+
+
+
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCanceledListener {
+
+                    }
+
+                }else {
+                    Toast.makeText(this,"Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else {
+                Toast.makeText(this,"Empty Fields Are not Allowed!! ", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
