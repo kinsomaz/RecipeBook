0060522bf436a36e94debe9346781e46708b6a3a
kinsomaz <Kinsomaz87@gmail.com>
2023-07-28 16:32:53 +0100

    update

---
+++ app/src/main/java/com/samad/recipebook/AppNameGlideModule.kt
@@ -0,0 +1,14 @@
+package com.samad.recipebook
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+class AppNameGlideModule : AppGlideModule(){
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.apply { RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL) }
+    }
+}
---
+++ app/src/main/java/com/samad/recipebook/RecipePageContainerAdapter.kt
@@ -0,0 +1,21 @@
+package com.samad.recipebook
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class RecipePageContainerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle) {
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+
+    override fun createFragment(position: Int): Fragment {
+       return if (position == 0)
+            RecipePage()
+        else
+            RecipePage_II()
+    }
+}
